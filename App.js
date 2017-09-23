d6311cf2ffadb35d6590a8bf4ecba97db7e0e00a
Josh Hawkins <jkhaccounts@gmail.com>
2017-09-23 15:45:15 -0500

    Add authentication

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Expo from "expo";
-import { Container, Header, Title } from "native-base";
+import { Container, Header, Title, Button, Text } from "native-base";
 import Map from "./components/map";
+import firebase from "./lib/firebase";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      error: false,
+      auth: false,
+      canceled: false
+    };
+  }
+
   async componentWillMount() {
     await Expo.Font.loadAsync({
       Roboto: require("native-base/Fonts/Roboto.ttf"),
@@ -11,13 +22,31 @@ export default class App extends React.Component {
     });
   }
 
+  async signIn() {
+    const result = await firebase.signInWithGoogle();
+
+    if (result.error) this.setState({ error: true });
+    else if (result.canceled) this.setState({ canceled: true });
+    else this.setState({ auth: true });
+  }
+
   render() {
+    const { auth, error, canceled } = this.state;
+
     return (
       <Container>
         <Header>
           <Title>Mappit</Title>
         </Header>
+        <Button onPress={this.signIn.bind(this)}>
+          <Text>Sign in with Google</Text>
+        </Button>
         <Map />
+        <Text>
+          {auth ? "Logged in" : null}
+          {error ? "An error occurred" : null}
+          {canceled ? "Canceled login" : ""}
+        </Text>
       </Container>
     );
   }
