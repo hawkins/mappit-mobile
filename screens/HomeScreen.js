5ae79f0c07bd993665cd97ffa7ba937fd6286be5
Josh Hawkins <jkhaccounts@gmail.com>
2017-09-23 18:11:40 -0500

    Add initial FAB

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,54 @@
 import React from "react";
-import { Icon, View, Button } from "native-base";
+import { Icon, View, Button, Fab, Container, Content } from "native-base";
 
 // Project Imports
 import Map from "../components/map";
 import HomeBanner from "../components/HomeBanner";
 
-const HomeScreen = ({ openDrawer, closeDrawer }) => (
-  <View>
-    <HomeBanner>
-      <Button transparent onPress={openDrawer}>
-        <Icon name="menu" style={{ color: "white" }} />
-      </Button>
-    </HomeBanner>
-    <Map />
-  </View>
-);
+class HomeScreen extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      active: false
+    };
+  }
+
+  render() {
+    const { openDrawer, closeDrawer } = this.props;
+
+    return (
+      <Container>
+        <HomeBanner>
+          <Button transparent onPress={openDrawer}>
+            <Icon name="menu" style={{ color: "white" }} />
+          </Button>
+        </HomeBanner>
+        <Container>
+          <Map />
+        </Container>
+        <Fab
+          active={this.state.active}
+          direction="up"
+          containerStyle={{}}
+          style={{ backgroundColor: "#5067FF" }}
+          position="bottomRight"
+          onPress={() => this.setState({ active: !this.state.active })}
+        >
+          <Icon name="share" />
+          <Button style={{ backgroundColor: "#34A34F" }}>
+            <Icon name="logo-whatsapp" />
+          </Button>
+          <Button style={{ backgroundColor: "#3B5998" }}>
+            <Icon name="logo-facebook" />
+          </Button>
+          <Button disabled style={{ backgroundColor: "#DD5144" }}>
+            <Icon name="mail" />
+          </Button>
+        </Fab>
+      </Container>
+    );
+  }
+}
 
 export default HomeScreen;
